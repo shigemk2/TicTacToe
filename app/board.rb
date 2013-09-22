d2f63be343fc0abb8c793e441bba200f21d8cf56
Michihito Shigemura <i.am.shige@gmail.com>
2013-09-22 15:33:13 +0900

    Delete trailing whitespace

--- app/board.rb
+++ app/board.rb
@@ -1,5 +1,5 @@
 class Board
-  
+
   # @grid:
   # +---+---+---+
   # | 0 | 1 | 2 |
@@ -8,29 +8,29 @@ class Board
   # +---+---+---+
   # | 6 | 7 | 8 |
   # +---+---+---+
-  
+
   attr_reader :grid
-  
+
   def initialize
     # -1 for unset squares
     @grid = Array.new(9, -1)
   end
-  
+
   # returns false if illegal move
   # and player id if it is legal
   def move(square, player)
     # check if square is taken
-    if not @grid[square] == -1 
-      return false      
+    if not @grid[square] == -1
+      return false
     end
     @grid[square] = player
   end
-  
+
   # resets the grid
-  def reset 
+  def reset
     @grid.replace(Array.new(9, -1))
   end
-  
+
   # returns the id of the winner
   # -1 for stalemate or false if
   # game is not over
@@ -42,17 +42,17 @@ class Board
         return @grid[i]
       end
     end
-    
+
     if @grid[0] != -1 && @grid[0] == @grid[4] && @grid[4] == @grid[8]
       return @grid[0]
     elsif @grid[2] != -1 && @grid[2] == @grid[4] && @grid[4] == @grid[6]
       return @grid[2]
     end
-    
+
     if not @grid.include?(-1)
       return -1
     end
-    
+
     false
   end
 
--- app/board_view_controller.rb
+++ app/board_view_controller.rb
@@ -1,20 +1,20 @@
 class BoardViewController < UIViewController
   SQUARE_SIZE = 80
   COLORS = [UIColor.greenColor, UIColor.redColor, UIColor.whiteColor]
-  
-  def viewDidLoad   
+
+  def viewDidLoad
     self.init_views
-    self.new_game 
+    self.new_game
     self.board_changed
   end
-  
+
   def init_views
     view.backgroundColor = UIColor.blackColor
-    
+
     # view for the board
     @board_view = UIView.alloc.initWithFrame([[0, 0], [SQUARE_SIZE * 3, SQUARE_SIZE * 3]])
     @board_view.center = view.center
-    
+
     # views for the square
     @square_views = []
     for i in 0..2
@@ -25,44 +25,44 @@ class BoardViewController < UIViewController
         @board_view.addSubview(square_view)
       end
     end
-    
+
     # label for game information
     @label = UILabel.alloc.initWithFrame([[0, 0], [0, 0]])
     @label.textColor = UIColor.whiteColor
     @label.font = UIFont.systemFontOfSize(30)
     @label.backgroundColor = nil
-    
+
     view.addSubview(@label)
     view.addSubview(@board_view)
   end
-  
+
   def show_info(text)
     @label.text = text
     @label.sizeToFit
     @label.center = [160, 50]
   end
-  
+
   def new_game
     # initalize the game
     @board = Board.new
     @current_player = 0
-  end  
-  
+  end
+
   def touchesEnded(touches, withEvent:event)
     # check if we're touching a square
     for i in 0..8
       if event.touchesForView(@square_views[i])
-        if not @board.move(i, @current_player) then 
+        if not @board.move(i, @current_player) then
           show_info("Illegal move!")
           break
         end
-        
+
         @current_player += 1
         @current_player %= 2
         break
       end
     end
-    
+
     # check if the game is over
     if winner = @board.game_over?
       @board.reset
@@ -72,15 +72,15 @@ class BoardViewController < UIViewController
         show_info("Player #{winner + 1} wins!")
       end
     end
-    
+
     self.board_changed
   end
-  
+
   # update the square views
   def board_changed
     for i in 0..8
       @square_views[i].backgroundColor = COLORS[@board.grid[i]]
     end
   end
-  
+
 end
